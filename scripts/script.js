354b38e94aa00c1e404f87bc013b09af45a2f2e1
dostonsobirov2020 <doston.sobirov.2020@mail.ru>
2020-05-13 00:02:07 +0500

    Sample exercises related to string methods were solved

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,30 @@
 let name = "Doston";
 let surname = "Sobirov";
+
+
+function ufFirst(str) {
+    if (!str) return str;
+
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+console.log(ufFirst('hello'));
+
+
+function truncate(str, maxlength) {
+    if (str.length > maxlength) {
+        str = str.slice(0, maxlength - 1) + '...';
+    }
+
+    return str;
+}
+
+console.log(truncate('Hello, how you guys doing? Could you guys please subscribe to my channel', 15));
+console.log(truncate('Hello, how are you?', 40));
+
+
+function extractCurrencyValue(value) {
+    return Number(value.slice(1));
+}
+
+console.log(extractCurrencyValue('$120') === 120);
