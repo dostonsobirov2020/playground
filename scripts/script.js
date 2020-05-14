0b3698da7b16c45536c0d9d0fbdb46b388ef82fd
dostonsobirov2020 <doston.sobirov.2020@mail.ru>
2020-05-14 12:04:42 +0500

    Played with scripts from net

--- scripts/script.js
+++ scripts/script.js
@@ -28,3 +28,29 @@ function extractCurrencyValue(value) {
 }
 
 console.log(extractCurrencyValue('$120') === 120);
+
+
+
+// Sum numbers from the visitor
+
+// let firstNumber = prompt('Enter first number');
+// let secondNumber = prompt('Enter second number');
+// alert(+firstNumber + +secondNumber);
+
+// Why 6.35.toFixed(1) == 6.3;
+
+// console.log(1.35.toFixed(1));
+// console.log(Math.round(6.35*10)/10);
+
+function myFunction() {
+    var a = 1;
+    if (a == 1) {
+        var b = 2;
+    }
+
+    return b;  // returns 2, would give an error if let was used instead of var;
+}
+
+var d = myFunction();
+
+console.log(d);
